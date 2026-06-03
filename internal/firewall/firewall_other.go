9122620e11a021b9c3455ffa3ad9a1c6578972ef
Ray <raynoldripper@gmail.com>
2026-06-03 17:23:32 +0300

    feat(firewall): non-linux stub

---
+++ internal/firewall/firewall_other.go
@@ -0,0 +1,20 @@
+//go:build !linux
+
+package firewall
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type stubFirewall struct{}
+
+// New returns a stub kill switch that errors on use. Real darwin/windows
+// firewall support is a later iteration.
+func New() Firewall { return stubFirewall{} }
+
+func (stubFirewall) On(Config) error {
+	return fmt.Errorf("firewall: kill switch not supported on %s yet", runtime.GOOS)
+}
+
+func (stubFirewall) Off() error { return nil }
